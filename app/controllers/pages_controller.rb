535376aab3fd2627164e25d83b4a16b9dd2eaf2e
davidpacecode <davidpace@gmail.com>
2025-05-12 22:20:42 -0500

    added some Puzzles and also finished homepage identifying current game number and game date

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def home
+    @current_puzzle_date = Puzzle.get_current_puzzle_date
+    @current_puzzle_number = Puzzle.get_current_puzzle_number
   end
 end
--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -3,4 +3,21 @@ class Puzzle < ApplicationRecord
 
   validates :puzzle_date, presence: true, uniqueness: true
   validates :puzzle_number, presence: true, uniqueness: true
+
+  before_validation :set_puzzle_number, on: :create
+
+  def self.get_current_puzzle_date
+    Puzzle.maximum(:puzzle_date)
+  end
+
+  def self.get_current_puzzle_number
+    Puzzle.maximum(:puzzle_number)
+  end
+
+  private
+
+  def set_puzzle_number
+    return if puzzle_number.present?
+    self.puzzle_number = Puzzle.maximum(:puzzle_number).to_i + 1
+  end
 end
